75daf6d902bd03c65a13292e8d5f4a95268cb449
Dhruv Kashyap <dhruv.kashyap1111@gmail.com>
2025-03-06 23:19:47 +0530

    admin logout added

--- Frontend/src/pages/Admin/Dashboard.jsx
+++ Frontend/src/pages/Admin/Dashboard.jsx
@@ -5,6 +5,7 @@ import Categories from "./Categories";
 import Inquiries from "./Inquiries";
 import CreateProduct from "./CreateProduct";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
   const [activeComponent, setActiveComponent] = useState("AllProducts");
@@ -26,9 +27,15 @@ const Dashboard = () => {
     }
   };
 
+  const logoutAdmin = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+    toast.success("Logout successful");
+  }
+  
   return (
     <div className="dashboard-container">
-      <div className="sidebar-container">
+      <div className="sidebar-container relative">
         <div className="logo-container">
           <h1 className="color">Admin</h1>
         </div>
@@ -38,6 +45,9 @@ const Dashboard = () => {
           <button className={`${activeComponent == "Create" && 'active' } secondary`} onClick={() => setActiveComponent("Create")}>Create Product</button>
           <button className={`${activeComponent == "Inquiries" && 'active' } secondary`} onClick={() => setActiveComponent("Inquiries")}>Inquiries</button>
           <button className={`${activeComponent == "Categories" && 'active' } secondary`} onClick={() => setActiveComponent("Categories")}>Categories</button>
+        </div>
+        <div className="logout-button absolute">
+          <button onClick={logoutAdmin} className="secondary" >Logout</button>
         </div>
       </div>
       <>
